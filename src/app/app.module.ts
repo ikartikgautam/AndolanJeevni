399a40dc2b1a72277f0c9f84f50d015bea365d12
Kaerik Gautam <gautam.kartik05@gmail.com>
2021-02-12 11:43:54 +0530

    home done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BottomNavBarComponent } from './bottom-nav-bar/bottom-nav-bar.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomNavBarComponent,
+    TopHeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
